b91b826becd4d3e227a8219083f65642934b9204
Angela <83084715+angela2009123@users.noreply.github.com>
2021-10-05 17:57:12 +0530

    Add files via upload

---
+++ practice-activity/activity1.js
@@ -0,0 +1,20 @@
+ score = 0;
+ function updateScore()
+{
+    score = score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
+function nextPage() {
+window.location = "actvity_2.html";
+}
+
+
+
+
+
+
+
